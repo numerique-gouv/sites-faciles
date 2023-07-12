f1618eade9725a6133a7cc27b1a5284a23b5ce85
Sébastien Reuiller <sebastien@reuiller.email>
2023-07-12 09:06:28 +0200

    fix migration conflict

--- content_manager/migrations/0006_analyticssettings.py
+++ content_manager/migrations/0006_analyticssettings.py
@@ -8,7 +8,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
     dependencies = [
         ("wagtailcore", "0078_referenceindex"),
-        ("content_manager", "0004_alter_contentpage_body"),
+        ("content_manager", "0005_alter_contentpage_body"),
     ]
 
     operations = [
