8cd0e41e012011bb496878da8b7255d3c8c12c1e
Sébastien Reuiller <sebastien@reuiller.email>
2023-09-01 11:59:09 +0200

    create formation list view

---
+++ formations/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from formations import views
+
+
+urlpatterns = [
+    path("catalogue/", views.FormationsListView.as_view(), name="formations_list"),
+]
---
+++ formations/views.py
@@ -0,0 +1,12 @@
+from django.views.generic.list import ListView
+
+from formations.models import FormationPage
+
+
+class FormationsListView(ListView):
+    model = FormationPage
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context["breadcrumb_data"] = {"current": "Catalogue"}
+        return context
