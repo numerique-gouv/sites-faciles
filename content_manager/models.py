821b7e8a5dbe5a9410513e0f128f9ab1c29118ad
Sylvain Boissel <sylvain@boissel.dev>
2023-11-16 18:44:30 +0100

    Récupération des changements faits sur le fork cms-suite-numerique (#48)
    * Create init.json
    
    * Update Procfile to load init on deploy
    
    * reset
    
    * update requirements to wagtail 5.1
    
    * remove willow
    
    willow is required by wagtail. I'll let wagtail handle it.
    
    * Add staticfiles/ folder to .gitignore
    
    * Update gitignore
    
    * Add a site config panel and adapt templates accordingly
    
    * update after review
    
    * Retrait de l'appel à init.json
    
    * change bucket config to align it with content-manager
    
    * Remove lines
    
    * Add lines
    
    ---------
    
    Co-authored-by: Marie <marie.jeammet@protonmail.com>
    Co-authored-by: Marie J <marie.jeammet@beta.gouv.fr>

--- content_manager/models.py
+++ content_manager/models.py
@@ -294,3 +294,70 @@ class AnalyticsSettings(BaseSiteSetting):
             heading="Scripts de suivi",
         ),
     ]
+
+
+@register_setting(icon="code")
+class CmsDsfrConfig(BaseSiteSetting):
+    class Meta:
+        verbose_name = "Configuration du site"
+
+    header_brand = models.CharField(
+        "Institution (en-tête)",
+        max_length=200,
+        default="Intitulé officiel",
+        help_text="""Intitulé du bloc-marques tel que défini sur la page
+        https://www.gouvernement.fr/charte/charte-graphique-les-fondamentaux/le-bloc-marque""",
+        blank=True,
+    )
+    header_brand_html = models.CharField(
+        "Institution avec césure (en-tête)",
+        max_length=200,
+        default="Intitulé<br />officiel",
+        blank=True,
+        help_text="""Intitulé du bloc-marques avec des balises <br />
+        pour affichage sur plusieurs lignes""",
+    )
+    footer_brand = models.CharField(
+        "Institution (pied)",
+        max_length=200,
+        default="Intitulé officiel",
+        blank=True,
+    )
+
+    footer_brand_html = models.CharField(
+        "Institution avec césure (pied)",
+        max_length=200,
+        default="Intitulé<br />officiel",
+        blank=True,
+    )
+
+    site_title = models.CharField(
+        "Titre du site",
+        max_length=200,
+        default="Titre du site",
+        blank=True,
+    )
+    site_tagline = models.CharField(
+        "Sous-titre du site",
+        max_length=200,
+        default="Sous-titre du site",
+        blank=True,
+    )
+    footer_description = models.TextField(
+        "Description",
+        default="",
+        blank=True,
+        help_text="Balises HTML autorisés",
+    )
+    mourning = models.BooleanField("Mise en berne", default=False)
+
+    panels = [
+        FieldPanel("header_brand"),
+        FieldPanel("header_brand_html"),
+        FieldPanel("footer_brand"),
+        FieldPanel("footer_brand_html"),
+        FieldPanel("site_title"),
+        FieldPanel("site_tagline"),
+        FieldPanel("footer_description"),
+        FieldPanel("mourning"),
+    ]
