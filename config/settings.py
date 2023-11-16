dd2a22fe3de5f3ca2c474977eb586cad291c5e0b
Sylvain Boissel <sylvain@boissel.dev>
2023-11-16 14:11:20 +0100

    Update AWS conf

--- config/settings.py
+++ config/settings.py
@@ -166,11 +166,11 @@ STATICFILES_FINDERS = [
 if os.getenv("S3_HOST"):
     AWS_S3_ACCESS_KEY_ID = os.getenv("S3_KEY_ID", "123")
     AWS_S3_SECRET_ACCESS_KEY = os.getenv("S3_KEY_SECRET", "secret")
-    AWS_S3_ENDPOINT_URL = os.getenv("S3_HOST")
+    AWS_S3_ENDPOINT_URL = f"{os.getenv('S3_PROTOCOL', 'https')}://{os.getenv('S3_HOST')}"
     AWS_STORAGE_BUCKET_NAME = os.getenv("S3_BUCKET_NAME", "set-bucket-name")
     AWS_S3_STORAGE_BUCKET_REGION = os.getenv("S3_BUCKET_REGION", "fr")
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-    MEDIA_URL = os.getenv("S3_HOST", "set-var-env.com/")
+    MEDIA_URL = f"{AWS_S3_ENDPOINT_URL}/"
 else:
     DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
     MEDIA_URL = "medias/"
