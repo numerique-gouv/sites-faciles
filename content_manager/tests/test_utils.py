987eed15658e9a18bf50c0d4cd67fc4bf88022cc
Sylvain Boissel <sylvain@boissel.dev>
2023-12-04 16:49:00 +0100

    Remove useless setup method

--- content_manager/tests/test_utils.py
+++ content_manager/tests/test_utils.py
@@ -1,27 +1,11 @@
-from django.contrib.auth.models import User
 from wagtail.images.models import Image
-from wagtail.models import Page
 from wagtail.test.utils import WagtailPageTestCase
 from wagtailmenus.models.menus import FlatMenu
 
-from content_manager.models import ContentPage
 from content_manager.utils import get_or_create_footer_menu, import_image
 
 
 class UtilsTestCase(WagtailPageTestCase):
-    def setUp(self):
-        home = Page.objects.get(slug="home")
-        self.admin = User.objects.create_superuser("test", "test@test.test", "pass")
-        self.admin.save()
-        self.content_page = home.add_child(
-            instance=ContentPage(
-                title="Page de contenu",
-                slug="content-page",
-                owner=self.admin,
-            )
-        )
-        self.content_page.save()
-
     def test_import_image(self):
         image_file = "static/artwork/technical-error.svg"
         image = import_image(image_file, "Sample image")
