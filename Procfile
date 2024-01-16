007263337cfa730fae0b2c2b940c6f9ddb5484fa
Sylvain Boissel <sylvain@boissel.dev>
2024-01-16 12:12:00 +0100

    Add new optional search page

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-postdeploy: python manage.py migrate
+postdeploy: python manage.py migrate && python manage.py update_index
 web: gunicorn config.wsgi --log-file -
--- content_manager/urls.py
+++ content_manager/urls.py
@@ -3,9 +3,12 @@ from wagtail import urls as wagtail_urls
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
+from content_manager.views import SearchResultsView
+
 
 urlpatterns = [
     path("cms-admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
+    path("recherche/", SearchResultsView.as_view(), name="cms_search"),
     path("", include(wagtail_urls)),
 ]
