3982523b24cf75a3db33a2a5fa0aca143f58d6f7
Sylvain Boissel <sylvain@boissel.dev>
2024-03-20 18:00:46 +0100

    Change test config

--- config/settings_test.py
+++ config/settings_test.py
@@ -1,5 +1,7 @@
 from config.settings import *  # NOSONAR # noqa: F401,F403
 
 
+DEBUG = True
+
 # Needed so that the conf is the same as the one where the blocks were generated
 SF_ALLOW_RAW_HTML_BLOCKS = True
