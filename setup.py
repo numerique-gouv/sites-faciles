a26d633cd6f27234cd30bd83a56abaeaf59a56c6
Sylvain Boissel <sylvain@boissel.dev>
2024-04-25 17:24:04 +0200

    Add .buildpacks config file and remove the now redundant setup.py

--- setup.py
+++
@@ -1,3 +0,0 @@
-# Empty file so that Scalingo detects that this is a python project
-# cf. https://doc.scalingo.com/platform/getting-started/common-deployment-errors#unknown-technology
-# Task for actual support of poetry in the Heroku buildpack https://github.com/heroku/buildpacks-python/issues/7
