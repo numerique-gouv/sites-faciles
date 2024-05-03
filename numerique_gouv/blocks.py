aef64973463ab53385717e8d11aeae8f0e9ce114
Lucien <mollard.lucien@gmail.com>
2024-05-03 18:14:32 +0200

    add translation and fix variable name

--- numerique_gouv/blocks.py
+++ numerique_gouv/blocks.py
@@ -5,28 +5,28 @@ from wagtail.images.blocks import ImageChooserBlock
 
 class ThreeCardsBlock(blocks.StructBlock):
     # Bloc avec trois cartes, une principale à gauche et deux secondaires à droite
-    main_image = ImageChooserBlock(required=True, help_text=_("Image de la carte principale"))
-    main_title = blocks.TextBlock(required=True, help_text=_("Titre de la carte principale"))
-    main_text = blocks.RichTextBlock(required=True, help_text=_("Texte de la carte principale"))
-    main_page_link = blocks.PageChooserBlock(required=False, help_text=_("Lien vers une page"))
-    main_url_link = blocks.URLBlock(required=False, help_text=_("Lien vers une URL externe"))
+    main_image = ImageChooserBlock(required=True, help_text=_("Main card image"))
+    main_title = blocks.TextBlock(required=True, help_text=_("Main card title"))
+    main_text = blocks.RichTextBlock(required=True, help_text=_("Main card text"))
+    main_page_link = blocks.PageChooserBlock(required=False, help_text=_("Link to a page"))
+    main_url_link = blocks.URLBlock(required=False, help_text=_("Link to an external URL"))
 
-    secondary_image = ImageChooserBlock(required=True, help_text=_("Image de la deuxième carte"))
-    secondary_text = blocks.RichTextBlock(required=True, help_text=_("Texte de la deuxième carte"))
-    secondary_button_label = blocks.TextBlock(required=True, help_text=_("Texte du bouton de la deuxième carte"))
-    secondary_page_link = blocks.PageChooserBlock(required=False, help_text=_("Lien vers une page"))
-    secondary_url_link = blocks.URLBlock(required=False, help_text=_("Lien vers une URL externe"))
+    top_right_image = ImageChooserBlock(required=True, help_text=_("Top right card image"))
+    top_right_text = blocks.RichTextBlock(required=True, help_text=_("Top right card text"))
+    top_right_button_label = blocks.TextBlock(required=True, help_text=_("Top right card button label"))
+    top_right_page_link = blocks.PageChooserBlock(required=False, help_text=_("Link to a page"))
+    top_right_url_link = blocks.URLBlock(required=False, help_text=_("Link to an external URL"))
 
-    tertiary_image = ImageChooserBlock(required=True, help_text=_("Image de la troisième carte"))
-    tertiary_text = blocks.RichTextBlock(required=True, help_text=_("Texte de la troisième carte"))
-    tertiary_button_label = blocks.TextBlock(required=True, help_text=_("Texte du bouton de la troisième carte"))
-    tertiary_page_link = blocks.PageChooserBlock(required=False, help_text=_("Lien vers une page"))
-    tertiary_url_link = blocks.URLBlock(required=False, help_text=_("Lien vers une URL externe"))
+    bottom_right_image = ImageChooserBlock(required=True, help_text=_("Bottom right card image"))
+    bottom_right_text = blocks.RichTextBlock(required=True, help_text=_("Bottom right card text"))
+    bottom_right_button_label = blocks.TextBlock(required=True, help_text=_("Bottom right card button label"))
+    bottom_right_page_link = blocks.PageChooserBlock(required=False, help_text=_("Link to a page"))
+    bottom_right_url_link = blocks.URLBlock(required=False, help_text=_("Link to an external URL"))
 
     class Meta:
         template = "numerique_gouv/blocks/three_cards.html"
         icon = "image"
-        label = "Unes"
+        label = "Headline cards"
 
 
-STREAMFIELD_NUMERIQUE_BLOCKS = [("three_cards", ThreeCardsBlock(label=_("Unes")))]
+STREAMFIELD_NUMERIQUE_BLOCKS = [("three_cards", ThreeCardsBlock(label=_("Headline cards")))]
