961b541280433fabd8171ea3d9ce1b42f33dc29a
Sylvain Boissel <sylvain@boissel.dev>
2024-10-17 16:43:46 +0200

    Add API entry for the child pages

--- content_manager/abstract.py
+++ content_manager/abstract.py
@@ -133,7 +133,14 @@ class SitesFacilesBasePage(Page):
         APIField("header_cta_buttons"),
         APIField("header_cta_label"),
         APIField("header_cta_link"),
+        APIField("public_child_pages"),
     ]
+
+    @property
+    def public_child_pages(self):
+        return [
+            {"id": child.id, "slug": child.slug, "title": child.title} for child in self.get_children().live().public()
+        ]
 
     def get_absolute_url(self):
         return self.url
