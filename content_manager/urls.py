9d0713c69587c7646d7a7ae44a910c8410d6b6dd
Sylvain Boissel <sylvain@boissel.dev>
2025-03-05 10:58:17 +0100

    Ajout d’une page "Plan du site" (#279)
    * First version
    
    * Fix tests
    
    * Update translations
    
    * Fix XML sitemap URL
    
    * Don't lazy load translations in URLS

--- content_manager/urls.py
+++ content_manager/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext as _
 from wagtail import urls as wagtail_urls
 
-from content_manager.views import SearchResultsView, TagsListView, TagView
+from content_manager.views import SearchResultsView, SiteMapView, TagsListView, TagView
 
 urlpatterns = [
     path(_("search/"), SearchResultsView.as_view(), name="cms_search"),
+    path(_("sitemap/"), SiteMapView.as_view(), name="readable_sitemap"),
     path("tags/<str:tag>/", TagView.as_view(), name="global_tag"),
     path("tags/", TagsListView.as_view(), name="global_tags_list"),
     path("", include(wagtail_urls)),
