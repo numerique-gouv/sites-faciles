1e6bc8a06e46e50997a841d37c72f7757d4b2652
Sébastien Reuiller <sebastien@reuiller.email>
2025-04-11 15:55:17 +0200

    now use pyairtable to work with new wagtail-airtable version

--- formations/utils.py
+++ formations/utils.py
@@ -1,6 +1,6 @@
 from content_manager.models import ContentPage
 
 
-def get_parent_page_of_formation_page(instance):
+def get_parent_page_of_formation_page():
     page = ContentPage.objects.get(slug="catalogue")
     return page.pk
