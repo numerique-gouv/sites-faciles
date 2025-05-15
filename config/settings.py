e20271d7ccc40f0e99a489f5e95be24685427084
Sylvain Boissel <sylvain@boissel.dev>
2025-05-15 17:59:30 +0200

    Increase min_length for passwords

--- config/settings.py
+++ config/settings.py
@@ -176,6 +176,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {
+            "min_length": 15,
+        },
     },
     {
         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
