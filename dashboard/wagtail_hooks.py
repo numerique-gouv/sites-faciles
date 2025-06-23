50f3107e0f26c374397295dadb3421b780ec1804
Lucie Laporte <lucielefrapper@macbookpro.home>
2025-06-23 11:41:34 +0200

    mix all script

--- dashboard/wagtail_hooks.py
+++ dashboard/wagtail_hooks.py
@@ -15,13 +15,15 @@ def global_admin_css():
 
 
 @hooks.register("insert_editor_js")
-def editor_js():
-    return format_html('\n<script src="{}"></script>', static("js/admin_editor.js"))
-
-
-@hooks.register("insert_editor_js")
-def insert_preview_panel_opener():
-    return format_html('<script src="{}"></script>', static("js/open_preview_panel.js"))
+def insert_custom_editor_scripts():
+    return format_html(
+        """
+        <script src="{}"></script>
+        <script src="{}"></script>
+        """,
+        static("js/admin_editor.js"),
+        static("js/open_preview_panel.js"),
+    )
 
 
 @hooks.register("register_admin_menu_item")
