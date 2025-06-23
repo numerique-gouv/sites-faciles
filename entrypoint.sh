e3dfd9600dd136d431b93e5bf0d57fda7e78357c
Sylvain Boissel <sylvain@boissel.dev>
2025-06-23 16:59:50 +0200

    Mise à jour des scripts de déploiement (#360)
    * Move from Poetry to uv
    
    * Move quality check to a separate CI action
    
    * Update CI
    
    * Update Docker conf
    
    * Move from Poetry to uv
    
    * Move quality check to a separate CI action
    
    * Update CI
    
    * Update Docker conf
    
    * Update deployment scripts
    
    * Update procfile
    
    * Remove obsolete files
    
    * Update Procfile
    
    * Fix menu link

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -ex
 export USE_UV=0
 export USE_DOCKER=0 # Commands run from inside docker shouldn't be prefixed
 
-just first-deploy
+just deploy
 
 if [ "$DEBUG" = "True" ]; then
     python manage.py runserver 0.0.0.0:$CONTAINER_PORT
