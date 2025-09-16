60a46d4fabf16dcd9b18d360715a55247fc2ee20
Lucie Laporte <lucielefrapper@macbookpro.home>
2025-09-16 19:14:53 +0200

    fix on templating file

--- content_manager/blocks.py
+++ content_manager/blocks.py
@@ -1356,7 +1356,7 @@ class ImageAndTextGrid(blocks.StructBlock):
     # layout = LayoutBlock()
 
     class Meta:
-        template = "content_manager/blocks/sections/imgage_text_grid.html"
+        template = "content_manager/blocks/sections/image_text_grid.html"
 
 
 STREAMFIELD_COMMON_BLOCKS = [
