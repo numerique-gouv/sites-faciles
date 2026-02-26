78e3ddb79b8a729773783e2993dc2715a73ce7bf
Lucie Laporte <lucielefrapper@macbookpro.home>
2026-02-26 18:59:43 +0100

    check tags only on live page without uv lock

--- content_manager/managers.py
+++ content_manager/managers.py
@@ -1,7 +1,19 @@
 from django.db import models
-from django.db.models import Count
+from django.db.models import Count, Q
 
 
 class TagManager(models.Manager):
+    """
+    Custom manager for Tag model.
+
+    Add a method to get tags with a minimum use count on live pages only.
+    """
+
     def tags_with_usecount(self, min_count=0):
-        return self.annotate(usecount=Count("contentpage")).filter(usecount__gte=min_count)
+        return self.annotate(
+            usecount=Count(
+                "content_manager_tagcontentpage_items",
+                filter=Q(content_manager_tagcontentpage_items__content_object__live=True),
+                distinct=True,
+            )
+        ).filter(usecount__gte=min_count)
