46cc0f01c6b93052d35bc3305a8e71e91d971ecd
Fabien Le Frapper <contact@fabienlefrapper.me>
2026-03-24 12:43:01 +0100

    Exclude from settings menu

--- menus/wagtail_hooks.py
+++ menus/wagtail_hooks.py
@@ -1,8 +1,22 @@
 from wagtail import hooks
 
+from menus.models import FooterBottomMenu, MainMenu, TopMenu
+
 from .views import MenusViewSetGroup
 
 
 @hooks.register("register_admin_viewset")
 def register_viewset():
     return MenusViewSetGroup()
+
+
+@hooks.register("construct_settings_menu")
+def exclude_menus_from_settings_menu(request, menu_items):
+    for index, item in enumerate(menu_items):
+        try:
+            if item.model in [TopMenu, FooterBottomMenu, MainMenu]:
+                del menu_items[index]
+        except AttributeError:
+            # Prevent the loop to choke on some menu items that
+            # do not specify model, like Redirect menu item
+            pass
