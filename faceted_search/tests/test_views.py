535fca55e6e0a268d6b045e04cfc7450bce30fd2
Estelle Comment <estelle.comment@beta.gouv.fr>
2026-09-07 14:57:36 +0200

    Better tests

--- faceted_search/tests/test_views.py
+++ faceted_search/tests/test_views.py
@@ -7,15 +7,12 @@ from urllib.parse import urlencode
 from bs4 import BeautifulSoup
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AnonymousUser
-from django.http import QueryDict
-from django.test import RequestFactory, SimpleTestCase
+from django.test import RequestFactory
 from django.urls import reverse
 from wagtail.models import Page, Site
 from wagtail.rich_text import RichText
 from wagtail.test.utils import WagtailPageTestCase
 
-from faceted_search.forms import FacetedSearchForm
-from faceted_search.search import RANK_BY_DATE, RANK_BY_RELEVANCE
 from faceted_search.tests.test_facets import FacetedSearchTestBase, get_post_titles_in_response
 from faceted_search.views import FacetedSearchResultsView
 from publications.tests.factories import PublicationIndexPageFactory, PublicationPageFactory
@@ -120,6 +117,14 @@ class FacetedSearchPaginationTest(FacetedSearchPaginationTestBase):
         self.assertIn("15 résultats", text)
         self.assertIn("10 par page", text)
 
+    def test_search_form_carries_no_page_control(self):
+        """Submitting the form drops ``page``, so a new search starts at page 1."""
+        response = self.client.get(self.search_url(page=2))
+        soup = BeautifulSoup(response.content, "html.parser")
+        form = soup.find("form", id="faceted-search-form")
+        self.assertIsNotNone(form)
+        self.assertEqual(form.select('[name="page"]'), [])
+
     def test_pagination_result_numbering_continues_across_pages(self):
         response = self.client.get(self.search_url(page=2))
         soup = BeautifulSoup(response.content, "html.parser")
@@ -160,39 +165,6 @@ class AccentInsensitiveSearchTest(FacetedSearchPaginationTestBase):
         unaccented = get_post_titles_in_response(self.client.get(self.search_url(query="ble")))
         self.assertEqual(accented, unaccented)
         self.assertIn(self.wheat_page.title, accented)
-
-
-class RankByParamTest(SimpleTestCase):
-    """No DB: the form is the only source of the ``rank_by`` value."""
-
-    def test_rank_by_value_and_default(self):
-        for query_string, expected in (
-            ("", RANK_BY_RELEVANCE),  # default is relevance
-            ("rank_by=relevance", RANK_BY_RELEVANCE),
-            ("rank_by=date", RANK_BY_DATE),
-            ("rank_by=popularity", RANK_BY_RELEVANCE),  # invalid values default to relevance
-        ):
-            with self.subTest(query_string=query_string):
-                form = FacetedSearchForm(QueryDict(query_string))
-                self.assertTrue(form.is_valid())
-                self.assertEqual(form.cleaned_data["rank_by"], expected)
-                # The checked radio must agree with the value the view ranks by.
-                self.assertEqual(form["rank_by"].value(), expected)
-
-    def test_rank_by_default_without_data(self):
-        self.assertEqual(FacetedSearchForm()["rank_by"].value(), RANK_BY_RELEVANCE)
-
-    def test_form_binds_query_and_ranking_and_ignores_page(self):
-        form = FacetedSearchForm(QueryDict("q=Report&page=2&rank_by=date"))
-        self.assertEqual(form["q"].value(), "Report")
-        self.assertEqual(form["rank_by"].value(), RANK_BY_DATE)
-        # page is not a form field, so submitting the form resets pagination.
-        self.assertNotIn("page", form.fields)
-
-    def test_form_drops_invalid_years(self):
-        form = FacetedSearchForm(QueryDict("q=Report&year=2024&year=nope"))
-        self.assertTrue(form.is_valid())
-        self.assertEqual(form.cleaned_data["year"], ["2024"])
 
 
 class FacetedSearchRankingTest(FacetedSearchPaginationTestBase):
